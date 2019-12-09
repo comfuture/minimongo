422a8cf1e529e015bc5e16c7267b27487b78f025
comfuture <comfuture@gmail.com>
2019-12-09 16:28:40 +0900

    remove unsed _connect param from MongoClientt

--- minimongo/model.py
+++ minimongo/model.py
@@ -58,7 +58,7 @@ class ModelBase(type):
             # creates :class:`pymongo.connection.Connection` object without
             # establishing connection. It's required if there is no running
             # mongodb at this time but we want to create :class:`Model`.
-            connection = MongoClient(*hostport, _connect=False)
+            connection = MongoClient(*hostport)
             mcs._connections[hostport] = connection
 
         new_class._meta = options
